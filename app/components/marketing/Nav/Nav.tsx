d1c89ec8403dadc06acae76e6b1a66c425bc2c68
Kevin Utoft <kevin.utoft@gmail.com>
2022-06-06 12:11:20 -0500

    add user context to landing header

--- app/components/marketing/Nav/Nav.tsx
+++ app/components/marketing/Nav/Nav.tsx
@@ -1,25 +1,44 @@
 import { NavLink } from "@remix-run/react"
+import { useMemo } from "react"
+import { Auth0Profile } from "remix-auth-auth0"
 import styles from "./styles.css"
 
 export const links = () => {
   return [{ rel: "stylesheet", href: styles }]
 }
 
-export const Nav = () => {
-  const routes = [
-    {
-      to: "/faq",
-      label: "FAQs",
-    },
-    {
-      to: "https://www.pokt.network/",
-      label: "About Pocket",
-    },
-    {
-      to: "https://docs.pokt.network/home/paths/app-developer",
-      label: "Docs",
-    },
-  ]
+type NavProps = {
+  user?: Auth0Profile
+}
+
+export const Nav = ({ user }: NavProps) => {
+  const routes = useMemo(() => {
+    const dashboardRoutes = [
+      {
+        to: "/dashboard",
+        label: "Dashboard",
+      },
+    ]
+    const marketingRoutes = [
+      {
+        to: "/faq",
+        label: "FAQs",
+      },
+      {
+        to: "https://www.pokt.network/",
+        label: "About Pocket",
+      },
+      {
+        to: "https://docs.pokt.network/home/paths/app-developer",
+        label: "Docs",
+      },
+    ]
+
+    if (user) {
+      return [...marketingRoutes, ...dashboardRoutes]
+    }
+    return marketingRoutes
+  }, [user])
 
   return (
     <nav>
--- app/routes/__landing.tsx
+++ app/routes/__landing.tsx
@@ -1,18 +1,31 @@
-import { Outlet } from "@remix-run/react"
+import { Outlet, useLoaderData } from "@remix-run/react"
 import { Header, links as HeaderLinks } from "~/components/shared/Header"
 import { Footer, links as FooterLinks } from "~/components/shared/Footer"
 import { Nav, links as NavLinks } from "~/components/marketing/Nav"
-import { LinksFunction } from "@remix-run/node"
+import { json, LinksFunction, LoaderFunction } from "@remix-run/node"
+import { Auth0Profile } from "remix-auth-auth0"
+import { getUserProfile } from "~/utils/session.server"
 
 export const links: LinksFunction = () => {
   return [...HeaderLinks(), ...FooterLinks(), ...NavLinks()]
 }
 
+type LoaderData = {
+  user: Awaited<Auth0Profile | undefined>
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({
+    user: await getUserProfile(request),
+  })
+}
+
 export default function LandingLayout() {
+  const data = useLoaderData() as LoaderData
   return (
     <>
-      <Header nav="right">
-        <Nav />
+      <Header nav="right" user={data.user}>
+        <Nav user={data.user} />
       </Header>
       <main>
         <Outlet />

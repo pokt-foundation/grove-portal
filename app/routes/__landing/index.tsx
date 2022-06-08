bc8a6545cf378794c45890a4f4f87f4b44310784
Hodak <10237065+jhodak@users.noreply.github.com>
2022-06-08 12:00:53 -0400

    refactor: cleanup and refactor to use multi-language

--- app/routes/__landing/index.tsx
+++ app/routes/__landing/index.tsx
@@ -3,6 +3,7 @@ import { Form } from "@remix-run/react"
 import { Grid } from "~/components/shared/Grid"
 import { LinksFunction } from "@remix-run/node"
 import { CallOutBox, links as CallOutBoxLinks } from "../../components/shared/CallOutBox"
+import { useTranslate } from "~/context/TranslateContext"
 import styles from "~/styles/landing.css"
 
 export const links: LinksFunction = () => {
@@ -10,71 +11,27 @@ export const links: LinksFunction = () => {
 }
 
 export default function Index() {
-  const text = {
-    chains: [
-      "avalanche",
-      "solana",
-      "algorand",
-      "okx",
-      "fuse",
-      "pokt",
-      "ethereum",
-      "polygon",
-      "gnosis",
-      "harmony",
-      "binance_smart_chain",
-      "iotex",
-      "boba",
-      "evmos",
-      "fantom",
-    ],
-    callOutBoxText: [
-      {
-        title: "One click Endpoints",
-        smallText: "For any network with 1M daily relays free",
-        blueText: "10+",
-        description: "Networks",
-      },
-      {
-        title: "Thousands of nodes",
-        smallText: "Serving any network at any given moment",
-        blueText: "47K+",
-        description: "Nodes",
-      },
-      {
-        title: "Monitor your Infra",
-        smallText: "Tracking and managing your app across any chain",
-        blueText: "6B+",
-        description: "Weekly relays",
-      },
-    ],
-    title: "Your gateway to Web3 done right.",
-    subtitle: `Deploy within minutes to decentralized infrastructure that can service
-    dozens of chains. The Portal acts as your one-stop-shop to manage, make
-    changes, and monitor your application's connection to blockchain data.`,
-    welcomeText: "Welcome to Web3 done the right way.",
-    getStarted: "Get Started",
-    connect: "Connect to these networks.",
-    whosNext: `See who's next.`,
-  }
+  const {
+    t: { landing },
+  } = useTranslate()
 
   return (
     <>
       <Container>
         <Grid grow>
           <Grid.Col sm={5}>
-            <h2>{text.title}</h2>
-            <p className="text">{text.subtitle}</p>
+            <h2>{landing.title}</h2>
+            <p className="text">{landing.subtitle}</p>
             <p className="text"></p>
             <Form action="/api/auth/auth0" method="post">
               <button type="submit" name="login" className="button" value="true">
-                {text.getStarted}
+                {landing.getStarted}
               </button>
             </Form>
           </Grid.Col>
 
           <Grid.Col sm={5} offset={2}>
-            {text.callOutBoxText.map((item) => {
+            {landing.callOutBoxText.map((item) => {
               return (
                 <CallOutBox
                   key={item.title}
@@ -88,16 +45,16 @@ export default function Index() {
           </Grid.Col>
           <Grid.Col>
             <p className="text">
-              {text.connect}{" "}
+              {landing.connect}{" "}
               <Form className="inline-form" action="/api/auth/auth0" method="post">
                 <button type="submit" name="signup" className="link" value="true">
-                  {text.whosNext}
+                  {landing.whosNext}
                 </button>
               </Form>
             </p>
           </Grid.Col>
           <div className="logo-box">
-            {text.chains.map(function (name) {
+            {landing.chains.map((name) => {
               return (
                 <img
                   key={name}

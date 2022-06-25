a20d2d0388d72cf5ef73405522c3010c73051fb6
Kevin Utoft <kevin.utoft@gmail.com>
2022-06-25 15:52:14 -0500

    make chart dynamic to lows and highs of the week

--- app/components/application/NetworkChartCard/formatDailyRelaysForGraphing.ts
+++ app/components/application/NetworkChartCard/formatDailyRelaysForGraphing.ts
@@ -13,20 +13,27 @@ export function formatDailyRelaysForGraphing(dailyRelays: DailyRelayBucket[] = [
     ({ bucket }) => DAYS[new Date(bucket.split("T")[0]).getUTCDay()],
   )
 
-  const highestDailyAmount = dailyRelays.reduce(
-    (highest, { total_relays: totalRelays }) => Math.max(highest, totalRelays),
-    0,
+  const { high, low } = dailyRelays.reduce(
+    ({ high: highest, low: lowest }, { total_relays: totalRelays }) => ({
+      high: Math.max(highest, totalRelays),
+      low: lowest === 0 ? totalRelays : Math.min(lowest, totalRelays),
+    }),
+    { high: 0, low: 0 },
   )
+
+  const diff = high - low
+  const quarter = diff * 0.25
+  const half = diff * 0.5
+  const threeQuarter = diff * 0.75
 
   const lines = [
     {
       id: 1,
       values: dailyRelays.map(({ total_relays: totalRelays }) =>
-        norm(totalRelays, 0, highestDailyAmount),
+        norm(totalRelays, low, high),
       ),
     },
   ]
-
   //   const formatSi = format(".2s")
 
   //   const scales = [
@@ -37,11 +44,11 @@ export function formatDailyRelaysForGraphing(dailyRelays: DailyRelayBucket[] = [
   //     { label: formatSi(highestDailyAmount.toFixed(0)) },
   //   ]
   const scales = [
-    { label: 0 },
-    { label: Number((highestDailyAmount * 0.25).toFixed(0)) },
-    { label: Number((highestDailyAmount * 0.5).toFixed(0)) },
-    { label: Number((highestDailyAmount * 0.75).toFixed(0)) },
-    { label: Number(highestDailyAmount.toFixed(0)) },
+    { label: Number(low.toFixed(0)) },
+    { label: Number((low + quarter).toFixed(0)) },
+    { label: Number((low + half).toFixed(0)) },
+    { label: Number((low + threeQuarter).toFixed(0)) },
+    { label: Number(high.toFixed(0)) },
   ]
 
   return {

91b46e6f74bfae7e5ef703322891f118de9e25c2
Hodak <10237065+jhodak@users.noreply.github.com>
2022-08-02 10:41:04 -0400

    fix: update testing slightly

--- app/components/application/AppAddressCard/AppAddressCard.test.tsx
+++ app/components/application/AppAddressCard/AppAddressCard.test.tsx
@@ -3,12 +3,12 @@ import AppAddressCard from "./AppAddressCard"
 import { render, screen } from "test/helpers"
 
 const apps = [{ appId: "123" }, { appId: "234" }]
+const value1 = apps[0].appId
+const value2 = apps[1].appId
 
 describe("<AppAddressCard />", () => {
   it("renders applications passed to it", () => {
     render(<AppAddressCard apps={apps} />)
-    const value1 = "123"
-    const value2 = "234"
     expect(screen.getByDisplayValue(value1)).toBeInTheDocument()
     expect(screen.getByDisplayValue(value2)).toBeInTheDocument()
   })
@@ -22,12 +22,6 @@ describe("<AppAddressCard />", () => {
     const numberOfApps = "2"
     expect(screen.queryByText(numberOfApps)).toBeInTheDocument()
   })
-  it("renders number of Apps", () => {
-    render(<AppAddressCard apps={apps} />)
-    const numberOfApps = "2"
-    expect(screen.queryByText(numberOfApps)).toBeInTheDocument()
-  })
-
   it("renders Errortext when no apps found", () => {
     render(<AppAddressCard apps={[]} />)
     const errorText = "No apps found."

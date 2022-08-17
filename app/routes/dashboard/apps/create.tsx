94c5ffd02e20892ff565ad1a36268966f7633201
Hodak <10237065+jhodak@users.noreply.github.com>
2022-08-17 11:10:46 -0400

    feat: setup create page designs

--- app/routes/dashboard/apps/create.tsx
+++ app/routes/dashboard/apps/create.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Group, Loader, Text, Title } from "@mantine/core"
+import { Box, Grid, Group, Loader, Radio, RadioGroup, Text, Title } from "@mantine/core"
 import {
   ActionFunction,
   json,
@@ -7,7 +7,8 @@ import {
   redirect,
 } from "@remix-run/node"
 import { Form, Link, useLoaderData, useActionData, useTransition } from "@remix-run/react"
-import { forwardRef } from "react"
+import clsx from "clsx"
+import { forwardRef, useState } from "react"
 import invariant from "tiny-invariant"
 import ChainWithImage, {
   AppEndpointProps,
@@ -20,6 +21,7 @@ import TextInput, { links as TextInputLinks } from "~/components/shared/TextInpu
 import { useFeatureFlags } from "~/context/FeatureFlagContext"
 import { initPortalClient } from "~/models/portal/portal.server"
 import { Stripe, stripe } from "~/models/stripe.server"
+import styles from "~/styles/dashboard.apps.create.css"
 import { AmplitudeEvents, trackEvent } from "~/utils/analytics"
 import { getErrorMessage } from "~/utils/catchError"
 import { CHAIN_ID_PREFIXES } from "~/utils/chainUtils"
@@ -33,7 +35,13 @@ export const meta: MetaFunction = () => {
 }
 
 export const links = () => {
-  return [...CardLinks(), ...TextInputLinks(), ...SelectLinks(), ...ChainWithImageLinks()]
+  return [
+    ...CardLinks(),
+    ...TextInputLinks(),
+    ...SelectLinks(),
+    ...ChainWithImageLinks(),
+    { rel: "stylesheet", href: styles },
+  ]
 }
 
 type LoaderData = {
@@ -130,34 +138,49 @@ export default function CreateApp() {
   const { price } = useLoaderData() as LoaderData
   const transition = useTransition()
   const action = useActionData() as ActionData
+  const [radioSelectedValue, setRadioSelectedValue] = useState("free")
 
   const tiers = [
     {
       name: "Always Free",
       value: "free",
       active: "true",
+      price: 0,
+      priceText: "$0.00",
+      cardDescription:
+        "Access to reliable, censor resistant infrastructure. Free up to 250k relays per day.",
     },
     {
       name: "Pay As You Go",
       value: "paid",
       active: flags.STRIPE_PAYMENT,
+      price: price?.tiers![1].unit_amount_decimal || 0.0000123,
+      priceText: "pay per relay",
+      cardDescription:
+        "250k free relays per day, per app. Beyond that, pay only for what you use.",
     },
     {
       name: "Enterprise",
       value: "enterprise",
       active: flags.ENTERPRISE,
+      price: 0,
+      priceText: "custom fit",
+      cardDescription: "Custom solution for large enterprises.",
     },
   ]
 
+  console.log(`flag stripe payment = ${flags.STRIPE_PAYMENT}`)
+
   return (
-    <section>
+    <section className="create-application">
       <Card>
         <div className="pokt-card-header">
           <h3>Create New App</h3>
         </div>
         <Form method="post">
           <TextInput label="Name" name="app-name" placeholder="New App Name" />
-          <Select
+          {/* // removing for now as it functionally serves almost no purpose
+            <Select
             searchable
             data={chains}
             filter={(value, item) =>
@@ -169,57 +192,47 @@ export default function CreateApp() {
             name="app-chain"
             nothingFound="No options"
             placeholder="Select Chain"
-          />
-          {/* TODO: add feature flag wrap */}
-          {price && (
-            <Box sx={{ textAlign: "center" }}>
-              <Title mb={16} mt={32} order={3}>
-                Flexible plans that grow with your app
-              </Title>
-              <Text mb={32}>
-                Scalable plans because your needs change as yous app grows. All plans
-                access to Pocket Network multichain infrastructure with our chain!
-              </Text>
-              <Grid align="center">
-                {tiers.map((tier, index) => (
-                  <Grid.Col key={tier.name} sm={4} xs={12}>
-                    <Card>
-                      <div>
-                        <h4>{tier.name}</h4>
-                        <p>
-                          {tier.active && price.tiers![index]
-                            ? price.tiers![index].unit_amount_decimal
-                            : "0"}
-                        </p>
-                      </div>
-                      <div>
-                        {tier.active === "true" ? (
-                          <Button
-                            disabled={transition.state === "submitting"}
-                            name="app-subscription"
-                            type="submit"
-                            value={tier.value}
-                            onClick={() => {
-                              trackEvent(AmplitudeEvents.EndpointCreation)
-                            }}
-                          >
-                            Select
-                          </Button>
-                        ) : (
-                          <Button disabled name="app-subscription" type="submit" value="">
-                            Coming Soon!
-                          </Button>
-                        )}
-                      </div>
-                    </Card>
-                  </Grid.Col>
-                ))}
-              </Grid>
-            </Box>
-          )}
-
-          <Text mb={16} mt={32} size="xs">
-            By using this application and the service, you agree to our{" "}
+          /> 
+          */}
+          <Grid align="center" className="radio-card-grid">
+            {tiers.map((tier) => (
+              <Grid.Col key={tier.name} sm={4} xs={12}>
+                <div
+                  className={clsx(
+                    "radio-card",
+                    tier.value === radioSelectedValue ? "active" : null,
+                  )}
+                  onClick={() =>
+                    tier.active === "true" && setRadioSelectedValue(tier.value)
+                  }
+                >
+                  <Radio
+                    readOnly
+                    checked={tier.value === radioSelectedValue}
+                    disabled={tier.active != "true"}
+                    label={tier.name}
+                    value={tier.name}
+                  />
+                  <p>{tier.priceText}</p>
+                  <p>{tier.cardDescription}</p>
+                </div>
+              </Grid.Col>
+            ))}
+          </Grid>
+          <Button
+            disabled={transition.state === "submitting"}
+            name="app-subscription"
+            type="submit"
+            value={radioSelectedValue}
+            variant="filled"
+            onClick={() => {
+              trackEvent(AmplitudeEvents.EndpointCreation)
+            }}
+          >
+            Create App
+          </Button>
+          <Text className="termsOfUseText" mb={16} mt={32} size="xs">
+            By using this application, you agree to our{" "}
             <a
               href="https://www.pokt.network/site-terms-of-use"
               rel="noreferrer"
@@ -229,19 +242,28 @@ export default function CreateApp() {
             </a>
             .
           </Text>
-          <Group align="center" position="apart">
-            {price ? (
-              <div></div>
-            ) : (
-              <Button name="subscription" type="submit" value="free">
-                Launch Application
-              </Button>
-            )}
-            <Button component={Link} to="/dashboard/apps" variant="subtle">
-              Back
-            </Button>
-          </Group>
         </Form>
+        <Box sx={{ textAlign: "left" }}>
+          <Title mb={16} mt={32} order={3}>
+            Flexible plans that grow with your app
+          </Title>
+          <Text mb={32}>
+            Scalable plans because your needs change as yous app grows. All plans access
+            to Pocket Network multichain infrastructure with our chain!
+          </Text>
+        </Box>
+        <Group align="center" position="apart">
+          {price ? (
+            <div></div>
+          ) : (
+            <Button name="subscription" type="submit" value="free">
+              Launch Application
+            </Button>
+          )}
+          <Button component={Link} to="/dashboard/apps" variant="subtle">
+            Back
+          </Button>
+        </Group>
       </Card>
       {action && (
         <Card>

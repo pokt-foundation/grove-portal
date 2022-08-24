f43c30940b4161f810b69fcacf67cde8862c371e
Hodak <10237065+jhodak@users.noreply.github.com>
2022-08-24 15:38:23 -0400

    fix: make tests better

--- app/components/application/StopRemoveApp/StopRemoveApp.test.tsx
+++ app/components/application/StopRemoveApp/StopRemoveApp.test.tsx
@@ -4,17 +4,24 @@ import { render, screen } from "test/helpers"
 import schema from "~/locales/en"
 import { PayPlanType } from "~/models/portal/sdk"
 
+const stopSubscription = schema.common.StopSubscription
+const removeApplication = "Remove Application"
+
 describe("<StopRemoveApp />", () => {
   it("renders Stop Subscription button for paid plantype", () => {
     render(<StopRemoveApp endpointId={"123"} planType={PayPlanType.PayAsYouGoV0} />)
-    const stopSubscription = schema.common.StopSubscription
+
     expect(screen.getByText(stopSubscription)).toBeInTheDocument()
     expect(screen.getByRole("link", { name: stopSubscription })).toBeInTheDocument()
+    expect(
+      screen.queryByRole("button", { name: removeApplication }),
+    ).not.toBeInTheDocument()
   })
   it("renders remove application for free tier plantype", () => {
     render(<StopRemoveApp endpointId={"123"} planType={PayPlanType.FreetierV0} />)
-    const removeApplication = "Remove Application"
+
     expect(screen.getByText(removeApplication)).toBeInTheDocument()
     expect(screen.getByRole("button", { name: removeApplication })).toBeInTheDocument()
+    expect(screen.queryByRole("link", { name: stopSubscription })).not.toBeInTheDocument()
   })
 })

3980d3c89d0b4313d52ce0008c32a85c07465969
Kevin Utoft <kevin.utoft@gmail.com>
2022-08-29 14:57:36 -0500

    add empty chart state

--- app/components/application/UsageChartCard/UsageChartCard.tsx
+++ app/components/application/UsageChartCard/UsageChartCard.tsx
@@ -1,8 +1,10 @@
 import { LineChart, useTheme } from "@pokt-foundation/ui"
+import { useMemo } from "react"
 import { formatDailyRelaysForGraphing } from "./formatDailyRelaysForGraphing"
 import styles from "./styles.css"
 import { Card, links as CardLinks } from "~/components/shared/Card"
 import { RelayMetric } from "~/models/relaymeter/relaymeter.server"
+import clsx from "clsx"
 
 /* c8 ignore start */
 export const links = () => {
@@ -23,6 +25,14 @@ export default function UsageChartCard({
 }: NetworkChardCardProps) {
   const theme = useTheme()
 
+  const hasRelays = useMemo(() => {
+    const r = relays.reduce((prev, curr) => {
+      return curr.Count.Total + prev
+    }, 0)
+
+    return r > 0
+  }, [relays])
+
   const { labels, lines, scales } = formatDailyRelaysForGraphing(relays)
   return (
     <div className="pokt-network-chart">
@@ -31,21 +41,29 @@ export default function UsageChartCard({
           <h3>{title}</h3>
           <p>{detail}</p>
         </div>
-        <LineChart
-          renderBackground
-          renderCheckpoints
-          renderHorizontalCheckLines
-          renderVerticalCheckLines
-          backgroundFill="#1B2331"
-          borderColor={`rgba(0,0,0,0)`}
-          color={() => theme.accentAlternative}
-          dotColor={theme.accent}
-          dotRadius={8}
-          height={240}
-          label={(index: number) => labels[index]}
-          lines={lines}
-          scales={scales}
-        />
+        <div className="pokt-chart-wrapper">
+          {!hasRelays && (
+            <div className="pokt-chart-overlay">
+              <p>Your applications do not have relay data yet.</p>
+            </div>
+          )}
+          <LineChart
+            renderBackground
+            renderCheckpoints
+            renderHorizontalCheckLines
+            renderVerticalCheckLines
+            backgroundFill="#1B2331"
+            borderColor={`rgba(0,0,0,0)`}
+            className={clsx(["pokt-chart-relays", { empty: !hasRelays }])}
+            color={() => theme.accentAlternative}
+            dotColor={hasRelays ? theme.accent : theme.disabled}
+            dotRadius={8}
+            height={180}
+            label={(index: number) => labels[index]}
+            lines={lines}
+            scales={scales}
+          />
+        </div>
       </Card>
     </div>
   )

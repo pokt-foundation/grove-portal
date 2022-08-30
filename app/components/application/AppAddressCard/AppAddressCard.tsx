fe29c9f2430961c7ea8319e45b07b508358cab4c
Kevin Utoft <kevin.utoft@gmail.com>
2022-08-30 15:45:29 -0500

    add collapse to app addresses card

--- app/components/application/AppAddressCard/AppAddressCard.tsx
+++ app/components/application/AppAddressCard/AppAddressCard.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import styles from "./styles.css"
 import { Card, links as CardLinks } from "~/components/shared/Card"
 import TextInput, { links as TextInputLinks } from "~/components/shared/TextInput"
 import { useTranslate } from "~/context/TranslateContext"
 import { ProcessedEndpoint } from "~/models/portal/sdk"
+import {
+  Button,
+  CaretDown,
+  CaretUp,
+  Collapse,
+  Divider,
+} from "@pokt-foundation/pocket-blocks"
+import clsx from "clsx"
 
 /* c8 ignore start */
 export const links = () => {
@@ -18,20 +27,52 @@ export default function AppAddressCard({ apps }: AppAddressCardProps) {
   const {
     t: { appAddressCard },
   } = useTranslate()
+  const [showAllApps, setShowAllApps] = useState(false)
+  const visibleApps = apps ? apps.slice(0, 3) : apps
+  const hiddenApps = apps ? apps.slice(3) : undefined
 
   return (
-    <div className="pokt-app-addresses">
+    <div className={clsx(["pokt-app-addresses", { "no-hidden-apps": hiddenApps }])}>
       <Card>
         <div className="flexContainer">
           <h3>{appAddressCard.heading}</h3>
           {apps && apps.length > 0 && <p>{apps.length}</p>}
         </div>
-        {apps && apps.length > 0 ? (
-          apps.map((item) =>
+        {visibleApps && visibleApps.length > 0 ? (
+          visibleApps.map((item) =>
             item ? <TextInput key={item.appId} copy readOnly value={item.appId} /> : null,
           )
         ) : (
           <p>{appAddressCard.error}</p>
+        )}
+        {hiddenApps && hiddenApps.length > 0 && (
+          <>
+            <Collapse in={showAllApps}>
+              {hiddenApps.map((item) =>
+                item ? (
+                  <TextInput key={item.appId} copy readOnly value={item.appId} />
+                ) : null,
+              )}
+            </Collapse>
+            <Divider
+              label={
+                <Button
+                  aria-label={showAllApps ? "Collapse" : "Expand"}
+                  className="pokt-button"
+                  size="small"
+                  variant="outline"
+                  onClick={() => setShowAllApps((c) => !c)}
+                >
+                  {showAllApps ? (
+                    <CaretUp className="pokt-icon" />
+                  ) : (
+                    <CaretDown className="pokt-icon" />
+                  )}
+                </Button>
+              }
+              labelPosition="center"
+            />
+          </>
         )}
       </Card>
     </div>

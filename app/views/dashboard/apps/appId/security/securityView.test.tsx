f4d3fd0ca61c9cefc0bd9e264b87f2d1791ebe60
Hodak <10237065+jhodak@users.noreply.github.com>
2022-08-30 13:11:22 -0400

    refactor: aliasing the text to be more descriptive

--- app/views/dashboard/apps/appId/security/securityView.test.tsx
+++ app/views/dashboard/apps/appId/security/securityView.test.tsx
@@ -1,5 +1,5 @@
 import { expect } from "vitest"
-import schema from "../../../../../locales/en"
+import englishText from "../../../../../locales/en"
 import SecurityView from "./securityView"
 import { render, screen } from "test/helpers"
 import { endpoint } from "~/models/portal/portal.data"
@@ -8,29 +8,41 @@ describe("<SecurityView />", () => {
   it("renders", () => {
     render(<SecurityView appId={"123123"} endpoint={endpoint} />)
     expect(
-      screen.getByRole("heading", { level: 3, name: schema.security.headings.secretKey }),
+      screen.getByRole("heading", {
+        level: 3,
+        name: englishText.security.headings.secretKey,
+      }),
     ).toBeInTheDocument()
     expect(
       screen.getByRole("heading", {
         level: 3,
-        name: schema.security.headings.approvedChains,
+        name: englishText.security.headings.approvedChains,
       }),
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("heading", { level: 3, name: schema.security.headings.contracts }),
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("heading", { level: 3, name: schema.security.headings.methods }),
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("heading", { level: 3, name: schema.security.headings.origins }),
     ).toBeInTheDocument()
     expect(
       screen.getByRole("heading", {
         level: 3,
-        name: schema.security.headings.userAgents,
+        name: englishText.security.headings.contracts,
       }),
     ).toBeInTheDocument()
-    expect(screen.getByText(schema.security.secretKeyText)).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", {
+        level: 3,
+        name: englishText.security.headings.methods,
+      }),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", {
+        level: 3,
+        name: englishText.security.headings.origins,
+      }),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("heading", {
+        level: 3,
+        name: englishText.security.headings.userAgents,
+      }),
+    ).toBeInTheDocument()
+    expect(screen.getByText(englishText.security.secretKeyText)).toBeInTheDocument()
   })
 })

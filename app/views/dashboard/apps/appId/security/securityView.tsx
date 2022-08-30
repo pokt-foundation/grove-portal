d40a928e93032802bd07207f06e690ab0a044ad9
Hodak <10237065+jhodak@users.noreply.github.com>
2022-08-30 13:07:09 -0400

    fix: lint changes

--- app/views/dashboard/apps/appId/security/securityView.tsx
+++ app/views/dashboard/apps/appId/security/securityView.tsx
@@ -1,4 +1,5 @@
 import { Text } from "@mantine/core"
+import { Button } from "@pokt-foundation/pocket-blocks"
 import { useFetcher, useParams } from "@remix-run/react"
 import React, { useState } from "react"
 import styles from "./styles.css"
@@ -8,7 +9,6 @@ import AppEndpointUrl, {
 import ChainsDropdown, {
   links as ChainsDropdownLinks,
 } from "~/components/application/ChainsDropdown/ChainsDropdown"
-import Button from "~/components/shared/Button"
 import Card, { links as CardLinks } from "~/components/shared/Card"
 import Switch, { links as SwitchLinks } from "~/components/shared/Switch"
 import TextInput, { links as TextInputLinks } from "~/components/shared/TextInput"

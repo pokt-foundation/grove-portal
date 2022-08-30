35087bef225e9e3618a7e41c61842f98a8f9405b
Kevin Utoft <kevin.utoft@gmail.com>
2022-08-30 11:50:07 -0500

    fix merge main

--- app/routes/dashboard/create.tsx
+++ app/routes/dashboard/create.tsx
@@ -1,4 +1,5 @@
-import { Box, Grid, Group, Loader, Text, Title } from "@mantine/core"
+import { Text } from "@mantine/core"
+import { Button } from "@pokt-foundation/pocket-blocks"
 import {
   ActionFunction,
   json,
@@ -6,25 +7,27 @@ import {
   MetaFunction,
   redirect,
 } from "@remix-run/node"
-import { Form, Link, useLoaderData, useActionData, useTransition } from "@remix-run/react"
-import { forwardRef, useEffect, useState } from "react"
+import { Form, useLoaderData, useActionData, useTransition } from "@remix-run/react"
+import { useEffect, useState } from "react"
 import invariant from "tiny-invariant"
-import ChainWithImage, {
-  AppEndpointProps,
-  links as ChainWithImageLinks,
-} from "~/components/application/ChainWithImage"
-import Button from "~/components/shared/Button"
+import AppPlansOverview, {
+  links as AppPlansOverviewLinks,
+} from "~/components/application/AppPlansOverview"
+import AppRadioCards, {
+  links as AppRadioCardsLinks,
+} from "~/components/application/AppRadioCards"
 import Card, { links as CardLinks } from "~/components/shared/Card"
-import Select, { links as SelectLinks } from "~/components/shared/Select"
 import TextInput, { links as TextInputLinks } from "~/components/shared/TextInput"
 import { useFeatureFlags } from "~/context/FeatureFlagContext"
 import { initPortalClient } from "~/models/portal/portal.server"
-import { Stripe, stripe } from "~/models/stripe.server"
+import { PayPlanType } from "~/models/portal/sdk"
+import { Stripe, stripe } from "~/models/stripe/stripe.server"
+import styles from "~/styles/dashboard.apps.create.css"
 import { AmplitudeEvents, trackEvent } from "~/utils/analytics"
 import { getErrorMessage } from "~/utils/catchError"
-import { CHAIN_ID_PREFIXES } from "~/utils/chainUtils"
 import { getRequiredServerEnvVar } from "~/utils/environment"
 import { requireUser } from "~/utils/session.server"
+import { getPlanName } from "~/utils/utils"
 
 export const meta: MetaFunction = () => {
   return {
@@ -33,7 +36,13 @@ export const meta: MetaFunction = () => {
 }
 
 export const links = () => {
-  return [...CardLinks(), ...TextInputLinks(), ...SelectLinks(), ...ChainWithImageLinks()]
+  return [
+    ...CardLinks(),
+    ...TextInputLinks(),
+    ...AppPlansOverviewLinks(),
+    ...AppRadioCardsLinks(),
+    { rel: "stylesheet", href: styles },
+  ]
 }
 
 type LoaderData = {
@@ -77,25 +86,24 @@ export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
   const subscription = formData.get("app-subscription")
   const name = formData.get("app-name")
-  const chain = formData.get("app-chain")
 
   invariant(
     subscription && typeof subscription === "string",
     "app subscription not found",
   )
   invariant(name && typeof name === "string", "app name not found")
-  invariant(chain && typeof chain === "string", "app name not found")
 
   try {
     const { createNewEndpoint } = await portal.createEndpoint({
       name,
+      payPlanType: subscription as PayPlanType,
     })
 
     if (!createNewEndpoint) {
       throw new Error("portal api could not create new endpoint")
     }
 
-    if (subscription === "paid") {
+    if (subscription === PayPlanType.PayAsYouGoV0) {
       formData.append("app-id", createNewEndpoint.id)
 
       // setting to any because of a TS nnown error: https://github.com/microsoft/TypeScript/issues/19806
@@ -112,24 +120,13 @@ export const action: ActionFunction = async ({ request }) => {
   }
 }
 
-const SelectItem = forwardRef<HTMLDivElement, AppEndpointProps>(
-  ({ chain, label, ...others }: AppEndpointProps, ref) => (
-    <div ref={ref} {...others}>
-      <ChainWithImage chain={chain} label={label} />
-    </div>
-  ),
-)
-
 export default function CreateApp() {
-  const chains = Array.from(CHAIN_ID_PREFIXES.entries()).map(([id, { name }]) => ({
-    chain: name,
-    label: name,
-    value: id,
-  }))
   const { flags } = useFeatureFlags()
   const { price } = useLoaderData() as LoaderData
   const transition = useTransition()
   const action = useActionData() as ActionData
+  const [radioSelectedValue, setRadioSelectedValue] = useState(PayPlanType.FreetierV0)
+  const [name, setName] = useState("")
   const [referral, setReferral] = useState("")
 
   useEffect(() => {
@@ -142,94 +139,57 @@ export default function CreateApp() {
 
   const tiers = [
     {
-      name: "Always Free",
-      value: "free",
+      name: getPlanName(PayPlanType.FreetierV0),
+      value: PayPlanType.FreetierV0,
       active: "true",
+      price: 0,
+      priceText: "$0.00",
+      cardDescription:
+        "Access to reliable, censor resistant infrastructure. Free up to 250k relays per day.",
     },
     {
-      name: "Pay As You Go",
-      value: "paid",
+      name: getPlanName(PayPlanType.PayAsYouGoV0),
+      value: PayPlanType.PayAsYouGoV0,
       active: flags.STRIPE_PAYMENT,
-    },
-    {
-      name: "Enterprise",
-      value: "enterprise",
-      active: flags.ENTERPRISE,
+      price: price?.tiers![1].unit_amount_decimal || 0.0000123,
+      priceText: "pay per relay",
+      cardDescription:
+        "250k free relays per day, per app. Beyond that, pay only for what you use.",
     },
   ]
 
   return (
-    <section>
+    <section className="create-application">
       <Card>
         <div className="pokt-card-header">
           <h3>Create New App</h3>
         </div>
         <Form method="post">
           <input hidden name="referral-id" type="text" value={referral} />
-          <TextInput label="Name" name="app-name" placeholder="New App Name" />
-          <Select
-            searchable
-            data={chains}
-            filter={(value, item) =>
-              item.chain.toLowerCase().includes(value.toLowerCase().trim()) ||
-              item.value.toLowerCase().includes(value.toLowerCase().trim())
-            }
-            itemComponent={SelectItem}
-            label="Chain"
-            name="app-chain"
-            nothingFound="No options"
-            placeholder="Select Chain"
+          <TextInput
+            label="Name"
+            name="app-name"
+            placeholder="New App Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
-          {/* TODO: add feature flag wrap */}
-          {price && (
-            <Box sx={{ textAlign: "center" }}>
-              <Title mb={16} mt={32} order={3}>
-                Flexible plans that grow with your app
-              </Title>
-              <Text mb={32}>
-                Scalable plans because your needs change as yous app grows. All plans
-                access to Pocket Network multichain infrastructure with our chain!
-              </Text>
-              <Grid align="center">
-                {tiers.map((tier, index) => (
-                  <Grid.Col key={tier.name} sm={4} xs={12}>
-                    <Card>
-                      <div>
-                        <h4>{tier.name}</h4>
-                        <p>
-                          {tier.active && price.tiers![index]
-                            ? price.tiers![index].unit_amount_decimal
-                            : "0"}
-                        </p>
-                      </div>
-                      <div>
-                        {tier.active === "true" ? (
-                          <Button
-                            disabled={transition.state === "submitting"}
-                            name="app-subscription"
-                            type="submit"
-                            value={tier.value}
-                            onClick={() => {
-                              trackEvent(AmplitudeEvents.EndpointCreation)
-                            }}
-                          >
-                            Select
-                          </Button>
-                        ) : (
-                          <Button disabled name="app-subscription" type="submit" value="">
-                            Coming Soon!
-                          </Button>
-                        )}
-                      </div>
-                    </Card>
-                  </Grid.Col>
-                ))}
-              </Grid>
-            </Box>
-          )}
-
-          <Text mb={16} mt={32} size="xs">
-            By using this application and the service, you agree to our{" "}
+          <AppRadioCards
+            currentRadio={radioSelectedValue}
+            radioData={tiers}
+            setRadio={setRadioSelectedValue}
+          />
+          <Button
+            disabled={transition.state === "submitting" || name === ""}
+            type="submit"
+            variant="filled"
+            onClick={() => {
+              trackEvent(AmplitudeEvents.EndpointCreation)
+            }}
+          >
+            Create App
+          </Button>
+          <Text className="termsOfUseText" mb={16} mt={32} size="xs">
+            By using this application, you agree to our{" "}
             <a
               href="https://www.pokt.network/site-terms-of-use"
               rel="noreferrer"
@@ -239,20 +199,14 @@ export default function CreateApp() {
             </a>
             .
           </Text>
-          <Group align="center" position="apart">
-            {price ? (
-              <div></div>
-            ) : (
-              <Button name="subscription" type="submit" value="free">
-                Launch Application
-              </Button>
-            )}
-            <Button component={Link} to="/dashboard/apps" variant="subtle">
-              Back
-            </Button>
-          </Group>
         </Form>
+        <AppPlansOverview planType={radioSelectedValue} />
+
+        {/* TO DO add in calculator component
+        <Calculator />
+              */}
       </Card>
+
       {action && (
         <Card>
           <p>{action.message}</p>

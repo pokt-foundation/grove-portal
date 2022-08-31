ecd738d102731b70fd336a4d0cc35da8279c0b9c
Hodak <10237065+jhodak@users.noreply.github.com>
2022-08-31 11:02:29 -0400

    fix: fix spelling

--- app/utils/analytics.ts
+++ app/utils/analytics.ts
@@ -11,7 +11,7 @@ export const AmplitudeEvents = {
   SupportView: "SUPPORT_VIEW",
   EndpointCreation: "ENDPOINT_CREATION",
   EndpointRemoval: "ENDPOINT_REMOVAL",
-  StopSubscription: "STOP_SUBCSCRIPTION",
+  StopSubscription: "STOP_SUBSCRIPTION",
   LandingView: "LANDING_VIEW",
   DashboardView: "DASHBOARD_VIEW",
   RequestDetailsView: "REQUEST_DETAILS_VIEW",

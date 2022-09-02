c4661c42b9a43d64c21a84f6463616e6a8161f99
Hodak <10237065+jhodak@users.noreply.github.com>
2022-09-02 11:44:26 -0400

    test: updated tests

--- app/components/application/AppKeysCard/AppKeysCard.test.tsx
+++ app/components/application/AppKeysCard/AppKeysCard.test.tsx
@@ -1,6 +1,6 @@
 import { expect } from "vitest"
 import AppKeysCard from "./AppKeysCard"
-import { render, screen } from "test/helpers"
+import { render, screen, userEvent, waitFor } from "test/helpers"
 
 const id = "123"
 const secret = "456"
@@ -46,5 +46,42 @@ describe("<AppKeysCard />", () => {
 
     expect(screen.getByText(label)).toBeInTheDocument()
     expect(screen.getByLabelText(label)).toHaveValue(publicKey)
+  })
+  it("displays inputs properly", () => {
+    render(<AppKeysCard id={id} publicKey={publicKey} secret={secret} />)
+    const showText = /Click to show value/i
+    const hideText = /Click to hide value/i
+    const copyText = /Click to copy/i
+    expect(screen.getAllByLabelText(showText)).toHaveLength(2)
+    expect(screen.queryByLabelText(hideText)).not.toBeInTheDocument()
+    expect(screen.getAllByLabelText(copyText)).toHaveLength(3)
+  })
+  it("shows and hides from revealIcon", async () => {
+    render(<AppKeysCard id={id} publicKey={publicKey} secret={secret} />)
+    const user = userEvent.setup()
+    const showText = /Click to show value/i
+    const hideText = /Click to hide value/i
+    const secretKey = /Secret key/i
+    expect(screen.getAllByLabelText(showText)).toHaveLength(2)
+    expect(screen.queryByLabelText(hideText)).not.toBeInTheDocument()
+
+    // click one of the show buttons
+    user.click(screen.getAllByLabelText(showText)[0])
+    await waitFor(() => {
+      expect(screen.getByLabelText(hideText)).toBeInTheDocument()
+    })
+
+    // click the remaining show button
+    user.click(screen.getAllByLabelText(showText)[0])
+    await waitFor(() => {
+      expect(screen.getAllByLabelText(hideText)).toHaveLength(2)
+    })
+
+    // click both hide buttons
+    user.click(screen.getAllByLabelText(hideText)[1])
+    user.click(screen.getAllByLabelText(hideText)[0])
+    await waitFor(() => {
+      expect(screen.getAllByLabelText(showText)).toHaveLength(2)
+    })
   })
 })

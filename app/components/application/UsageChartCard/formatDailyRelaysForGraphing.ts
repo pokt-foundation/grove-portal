45850155271d087b8b0b815d3eb8ba2b294c167b
Hodak <10237065+jhodak@users.noreply.github.com>
2022-09-15 14:34:44 -0400

    fix: fixes low logic problems when there are 0's for days

--- app/components/application/UsageChartCard/formatDailyRelaysForGraphing.ts
+++ app/components/application/UsageChartCard/formatDailyRelaysForGraphing.ts
@@ -1,4 +1,5 @@
 // import { format } from "d3-format"
+import { count } from "console"
 import { RelayMetric } from "~/models/relaymeter/relaymeter.server"
 import { dayjs } from "~/utils/dayjs"
 import { formatNumberToSICompact } from "~/utils/formattingUtils"
@@ -18,15 +19,27 @@ export function formatDailyRelaysForGraphing(relays: RelayMetric[] = []): {
     ({ From }) => DAYS[new Date(From.split("T")[0]).getUTCDay()],
   )
 
-  const { high, low } = sortRelaysByDate.reduce(
-    ({ high: highest, low: lowest }, { Count }) => ({
-      high: Math.max(highest, Count.Total),
-      low: lowest === 0 ? Count.Total : Math.min(lowest, Count.Total),
-    }),
-    { high: 0, low: 0 },
-  )
+  const findHighandLow = () => {
+    let high = 0
+    let low: number[] = []
+    sortRelaysByDate.map((item) => {
+      if (item.Count.Total >= high) {
+        high = item.Count.Total
+      }
+      if (low[0] === undefined) {
+        low[0] = item.Count.Total
+      } else if (item.Count.Total < low[0]) {
+        low[0] = item.Count.Total
+      } else if (item.Count.Total === 0) {
+        low.push(item.Count.Total)
+      }
+    })
+    return { high: high, low: low[0] }
+  }
 
-  const diff = high - low
+  const highest = findHighandLow().high
+  const lowest = findHighandLow().low
+  const diff = highest - lowest
   const quarter = diff * 0.25
   const half = diff * 0.5
   const threeQuarter = diff * 0.75
@@ -34,16 +47,16 @@ export function formatDailyRelaysForGraphing(relays: RelayMetric[] = []): {
   const lines = [
     {
       id: 1,
-      values: sortRelaysByDate.map(({ Count }) => norm(Count.Total, low, high)),
+      values: sortRelaysByDate.map(({ Count }) => norm(Count.Total, lowest, highest)),
     },
   ]
 
   const scales = [
-    { label: formatNumberToSICompact(Number(low.toFixed(0))) },
-    { label: formatNumberToSICompact(Number((low + quarter).toFixed(0))) },
-    { label: formatNumberToSICompact(Number((low + half).toFixed(0))) },
-    { label: formatNumberToSICompact(Number((low + threeQuarter).toFixed(0))) },
-    { label: formatNumberToSICompact(Number(high.toFixed(0))) },
+    { label: formatNumberToSICompact(Number(lowest.toFixed(0))) },
+    { label: formatNumberToSICompact(Number((lowest + quarter).toFixed(0))) },
+    { label: formatNumberToSICompact(Number((lowest + half).toFixed(0))) },
+    { label: formatNumberToSICompact(Number((lowest + threeQuarter).toFixed(0))) },
+    { label: formatNumberToSICompact(Number(highest.toFixed(0))) },
   ]
 
   return {

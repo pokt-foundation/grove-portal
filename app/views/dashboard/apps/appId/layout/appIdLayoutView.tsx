a22c593a46afa36c9927da27f4bcae0895ea3ce3
Hodak <10237065+jhodak@users.noreply.github.com>
2022-11-04 16:04:01 -0400

    fix: add event to amplitude when new subscriptions are created

--- app/views/dashboard/apps/appId/layout/appIdLayoutView.tsx
+++ app/views/dashboard/apps/appId/layout/appIdLayoutView.tsx
@@ -26,6 +26,7 @@ import { useFeatureFlags } from "~/context/FeatureFlagContext"
 import { useTranslate } from "~/context/TranslateContext"
 import { EndpointQuery, PayPlanType } from "~/models/portal/sdk"
 import { Stripe } from "~/models/stripe/stripe.server"
+import { AmplitudeEvents, trackEvent } from "~/utils/analytics"
 import { getRequiredClientEnvVar } from "~/utils/environment"
 import { getPlanName } from "~/utils/utils"
 
@@ -117,6 +118,7 @@ export default function AppIdLayoutView({
           },
         )
       }
+      // trackEvent(AmplitudeEvents.NewSubscription)
       setSearchParams({})
       setShowSuccessModel(true)
     }

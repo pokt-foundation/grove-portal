2edd707b44bbabd2fed079113ef108573bd0293c
Kevin Utoft <kevin.utoft@gmail.com>
2022-11-17 16:02:11 -0600

    add network view

--- app/routes/network.tsx
+++ app/routes/network.tsx
@@ -1,29 +1,5 @@
-import { Grid } from "@pokt-foundation/pocket-blocks"
 import { LoaderFunction, json, MetaFunction } from "@remix-run/node"
 import { useLoaderData, useTransition } from "@remix-run/react"
-import ChainWithImage, {
-  links as ChainWithImageLinks,
-} from "~/components/application/ChainWithImage"
-import FeedbackCard, {
-  links as FeedbackCardLinks,
-} from "~/components/application/FeedbackCard"
-import NetworkPoktScanLatestBlockCard, {
-  links as NetworkPoktScanLatestBlockCardLinks,
-} from "~/components/application/NetworkPoktScanLatestBlockCard"
-import NetworkRelayPerformanceCard, {
-  links as NetworkRelayPerformanceCardLinks,
-} from "~/components/application/NetworkRelayPerformanceCard"
-import NetworkSuccessRateCard, {
-  links as NetworkSuccessRateCardLinks,
-} from "~/components/application/NetworkSuccessRateCard"
-import NetworkSummaryCard, {
-  links as NetworkSummaryCardLinks,
-} from "~/components/application/NetworkSummaryCard"
-import UsageChartCard, {
-  links as UsageChartCardLinks,
-} from "~/components/application/UsageChartCard"
-import Loader, { links as LoaderLinks } from "~/components/shared/Loader"
-import Table, { links as TableLinks } from "~/components/shared/Table"
 import { Block, Order } from "~/models/indexer/sdk"
 import { initPoktScanClient } from "~/models/poktscan/poktscan.server"
 import { GetHighestBlockQuery } from "~/models/poktscan/sdk"
@@ -34,30 +10,16 @@ import {
   getRelaysPerWeek,
   RelayMetric,
 } from "~/models/relaymeter/relaymeter.server"
-import styles from "~/styles/dashboard.index.css"
-import { getServiceLevelByChain } from "~/utils/chainUtils"
 import { dayjs } from "~/utils/dayjs"
-import { requireUser } from "~/utils/session.server"
+import NetworkView, { links as NetworkViewLinks } from "~/views/network/networkView"
 
 export const links = () => {
-  return [
-    ...NetworkSummaryCardLinks(),
-    ...NetworkPoktScanLatestBlockCardLinks(),
-    // ...NetworkLatestBlockCardLinks(),
-    ...NetworkSuccessRateCardLinks(),
-    ...NetworkRelayPerformanceCardLinks(),
-    ...UsageChartCardLinks(),
-    ...TableLinks(),
-    ...ChainWithImageLinks(),
-    ...FeedbackCardLinks(),
-    ...LoaderLinks(),
-    { rel: "stylesheet", href: styles },
-  ]
+  return [...NetworkViewLinks()]
 }
 
 export const meta: MetaFunction = () => {
   return {
-    title: "Dashboard Home",
+    title: "Pocket Network Summary | POKT",
   }
 }
 
@@ -70,7 +32,7 @@ export type LatestBlockType = Block & {
   total_txs: number
 }
 
-type LoaderData = {
+export type NetworkLoaderData = {
   blockchains: Blockchain[] | null
   dailyNetworkRelaysPerWeek: RelayMetric[]
   dailyNetworkRelays: RelayMetric
@@ -111,7 +73,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const weeklyNetworkRelays = await getRelays("network", week, today)
   const monthlyNetworkRelays = await getRelays("network", month, today)
 
-  return json<LoaderData>(
+  return json<NetworkLoaderData>(
     {
       blockchains: blockchainResponse
         ? (blockchainResponse.blockchains.filter(
@@ -144,85 +106,18 @@ export default function Index() {
     weeklyNetworkRelays,
     // latestBlock,
     poktscanLatestBlock,
-  } = useLoaderData() as LoaderData
+  } = useLoaderData() as NetworkLoaderData
   const { state } = useTransition()
   return (
-    <>
-      {state === "loading" && <Loader />}
-      <Grid gutter={32}>
-        <Grid.Col md={8}>
-          <section>
-            <h3>Network Summary</h3>
-            <Grid gutter={32}>
-              <Grid.Col sm={4}>
-                <NetworkSummaryCard
-                  imgSrc="/networkSummaryNodes.png"
-                  subtitle="7000+"
-                  title="Nodes Staked"
-                />
-              </Grid.Col>
-              <Grid.Col sm={4}>
-                <NetworkSummaryCard
-                  imgSrc="/networkSummaryApps.png"
-                  subtitle="2000+"
-                  title="Apps Staked"
-                />
-              </Grid.Col>
-              <Grid.Col sm={4}>
-                <NetworkSummaryCard
-                  imgSrc="/networkSummaryNetworks.png"
-                  subtitle={String(blockchains ? blockchains.length : 0)}
-                  title="Networks"
-                />
-              </Grid.Col>
-            </Grid>
-          </section>
-          <section>
-            <UsageChartCard relays={dailyNetworkRelaysPerWeek} />
-          </section>
-          {blockchains && (
-            <section>
-              <Table
-                paginate
-                search
-                columns={["Network", "ID", "Status"]}
-                data={blockchains.map((chain) => ({
-                  id: chain.id,
-                  network: {
-                    value: `${chain.description}`,
-                    element: <ChainWithImage chain={chain.description} />,
-                  },
-                  chainId: chain.id,
-                  status: getServiceLevelByChain(chain.id),
-                }))}
-                label="Available Networks"
-              />
-            </section>
-          )}
-        </Grid.Col>
-        <Grid.Col md={4}>
-          <section>
-            <h3>Network Success Rate</h3>
-            <NetworkSuccessRateCard relays={weeklyNetworkRelays} />
-          </section>
-          {poktscanLatestBlock && (
-            <section>
-              <NetworkPoktScanLatestBlockCard latestBlock={poktscanLatestBlock} />
-            </section>
-          )}
-          <section>
-            <NetworkRelayPerformanceCard
-              month={monthlyNetworkRelays}
-              today={dailyNetworkRelays}
-              week={weeklyNetworkRelays}
-            />
-          </section>
-          <section>
-            <FeedbackCard />
-          </section>
-        </Grid.Col>
-      </Grid>
-    </>
+    <NetworkView
+      blockchains={blockchains}
+      dailyNetworkRelays={dailyNetworkRelays}
+      dailyNetworkRelaysPerWeek={dailyNetworkRelaysPerWeek}
+      monthlyNetworkRelays={monthlyNetworkRelays}
+      poktscanLatestBlock={poktscanLatestBlock}
+      state={state}
+      weeklyNetworkRelays={weeklyNetworkRelays}
+    />
   )
 }
 

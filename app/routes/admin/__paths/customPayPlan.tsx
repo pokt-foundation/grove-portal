4c0d4f4ed0a933612e23b84103df79c74a1af47a
Kevin Utoft <kevin.utoft@gmail.com>
2022-11-29 21:15:30 -0600

    updatePlan api to handle custom limit. update util fuctions to handle enterprise. add updateplan functionality to admin custom plan page

--- app/routes/admin/__paths/customPayPlan.tsx
+++ app/routes/admin/__paths/customPayPlan.tsx
@@ -1,48 +1,52 @@
-import { Avatar, Badge } from "@mantine/core"
-import { Button, Container, Select, Group } from "@pokt-foundation/pocket-blocks"
+import { Button, Container, Select } from "@pokt-foundation/pocket-blocks"
 import { LinksFunction } from "@remix-run/node"
+import { useFetcher } from "@remix-run/react"
 import { useState } from "react"
 import Card, { links as CardLinks } from "~/components/shared/Card"
 import TextInput, { links as TextInputLinks } from "~/components/shared/TextInput"
+import { PayPlanType } from "~/models/portal/sdk"
 
 export const links: LinksFunction = () => [...CardLinks(), ...TextInputLinks()]
 
 export default function CustomPayPlan() {
   const [selectedPlanType, setSelectedPlanType] = useState<string | null>(null)
+  const updatePlanFetcher = useFetcher()
 
-  const plans = ["Free", "Paid", "Custom"]
+  const plans = Object.values(PayPlanType)
 
   return (
     <Container size="lg">
       <Card>
         <div className="pokt-card-header">
-          <h3>Pay Plan Metrics</h3>
-        </div>
-        <Group>
-          {plans.map((plan) => (
-            <div key={plan}>{plan}</div>
-          ))}
-        </Group>
-      </Card>
-      <Card>
-        <div className="pokt-card-header">
           <h3>Update Pay Plan</h3>
         </div>
-        <Select
-          data={["Custom", "Free", "Test"]}
-          id="pay_plan_type"
-          label="Plan Type"
-          mb={16}
-          placeholder="Plan Type"
-          onChange={(e) => setSelectedPlanType(e)}
-        />
-        <TextInput label="Endpoint ID" mb={16} placeholder="Endpoint ID" />
-        {selectedPlanType === "Custom" && (
-          <TextInput label="Rate Limit" mb={16} placeholder="Rate Limit" type="number" />
+        <updatePlanFetcher.Form action="/api/updatePlan" method="post">
+          <Select
+            data={plans}
+            id="pay_plan_type"
+            label="Plan Type"
+            mb={16}
+            name="type"
+            placeholder="Plan Type"
+            onChange={(e) => setSelectedPlanType(e)}
+          />
+          <TextInput label="Endpoint ID" mb={16} name="id" placeholder="Endpoint ID" />
+          {selectedPlanType === PayPlanType.Enterprise && (
+            <TextInput
+              label="Rate Limit"
+              mb={16}
+              name="limit"
+              placeholder="Rate Limit"
+              type="number"
+            />
+          )}
+          <Button disabled={updatePlanFetcher.state !== "idle"} mt={16} type="submit">
+            Submit
+          </Button>
+        </updatePlanFetcher.Form>
+        {updatePlanFetcher.data && updatePlanFetcher.data.error && (
+          <div>{JSON.stringify(updatePlanFetcher.data)}</div>
         )}
-        <Button mt={16} type="submit">
-          Submit
-        </Button>
       </Card>
     </Container>
   )

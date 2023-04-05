9e7b8af34e81026827278aa8a0c4952857461cc3
PseudoMera <albinesteban@outlook.com>
2023-04-05 13:09:55 -0400

    feat: added more advanced markdown support

--- app/components/shared/Remark/Remark.tsx
+++ app/components/shared/Remark/Remark.tsx
@@ -1,4 +1,7 @@
-import { Remark as ReactRemark, RemarkProps } from "react-remark"
+import ReactMarkdown, { Options } from "react-markdown"
+// import { Remark as ReactRemark, RemarkProps } from "react-remark"
+import rehypeRaw from 'rehype-raw'
+import remarkGfm from "remark-gfm"
 import styles from "./styles.css"
 
 /* c8 ignore start */
@@ -7,26 +10,31 @@ export const links = () => {
 }
 /* c8 ignore stop */
 
-export const Remark = (props: RemarkProps) => {
+export const Remark = (props: Options) => {
   return (
-    <ReactRemark
-      rehypeReactOptions={{
-        components: {
-          a: (props: any) => (
-            <a
-              className="pokt-remark-a"
-              target={props.href.includes("http") ? "_blank" : "_self"}
-              {...props}
-            >
-              {props.children}
-            </a>
-          ),
-          img: (props: any) => <img className="pokt-remark-img" {...props} />,
-        },
-      }}
-    >
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
       {props.children}
-    </ReactRemark>
+    </ReactMarkdown>
+    // <ReactRemark
+    //   rehypeReactOptions={{
+    //     components: {
+    //       a: (props: any) => (
+    //         <a
+    //           className="pokt-remark-a"
+    //           target={props.href.includes("http") ? "_blank" : "_self"}
+    //           {...props}
+    //         >
+    //           {props.children}
+    //         </a>
+    //       ),
+    //       img: (props: any) => <img className="pokt-remark-img" {...props} />,
+    //     },
+    //   }}
+    //   //@ts-ignore
+    //   remarkPlugins={[remarkGfm]}
+    // >
+    //   {props.children}
+    // </ReactRemark>
   )
 }
 

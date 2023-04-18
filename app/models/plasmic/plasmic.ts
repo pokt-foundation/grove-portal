1d5eaf4dd75e992a2d7460cdae151e9b06062e49
PseudoMera <albinesteban@outlook.com>
2023-04-18 12:46:19 -0400

    feat: replace hard coded values with env

--- app/models/plasmic/plasmic.ts
+++ app/models/plasmic/plasmic.ts
@@ -1,13 +1,13 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-react"
+import { getRequiredServerEnvVar } from "~/utils/environment"
+
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: "7AMMaZfSfMAJorUAF1dxTi", // ID of a project you are using
-      token:
-        "2CqoU4qWlMAcxr9XwGzY5gvw4P5SlwIwgh8GWQ7WpyjD8xUnorL38im2HOFci8VoKjsOic73tSqFo5NHGIw", // API token for that project
+      id: getRequiredServerEnvVar("PLASMIC_ID"),
+      token: getRequiredServerEnvVar("PLASMIC_TOKEN"),
     },
   ],
-  // Fetches the latest revisions, whether or not they were unpublished!
-  // Disable for production to ensure you render only published changes.
+
   preview: true,
 })

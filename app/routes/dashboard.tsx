e9f2c699b87aa37119b5a36b29795ad5acb80e60
PseudoMera <albinesteban@outlook.com>
2023-04-24 11:42:18 -0400

    refactor: maintenance mode and added new maintenance env for dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,7 +2,10 @@ import { Box, Button, Center, Group, Text, Title } from "@pokt-foundation/pocket
 import { LinksFunction, LoaderFunction } from "@remix-run/node"
 import { Outlet, useCatch, Link } from "@remix-run/react"
 import styles from "~/styles/dashboard.css"
+import { getRequiredClientEnvVar } from "~/utils/environment"
 import { requireUserProfile } from "~/utils/session.server"
+
+const DASHBOARD_MAINTENANCE = getRequiredClientEnvVar("FLAG_MAINTENANCE_MODE_DASHBOARD")
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }]
@@ -14,25 +17,28 @@ export const loader: LoaderFunction = async ({ request }) => {
 }
 
 export default function Dashboard() {
-  return (
-    <Center className="error-container" mt="xl">
-      <Box sx={{ maxWidth: "600px", textAlign: "center" }}>
-        <Title order={1}>Scheduled Maintenance</Title>
-        <Text color="white" size="sm">
-          Our platform is currently undergoing scheduled maintenance today. During this
-          time, the Portal UI is temporarily unavailable, and users are unable to create
-          or edit applications, adjust security settings, or pay plans. However, rest
-          assured that all relay requests are being processed as usual.
-        </Text>
-        <Group position="center">
-          <Button component={Link} size="sm" to="/">
-            Portal Home
-          </Button>
-        </Group>
-      </Box>
-    </Center>
-  )
-  // return <Outlet />
+  if (DASHBOARD_MAINTENANCE === "true") {
+    return (
+      <Center className="error-container" mt="xl">
+        <Box sx={{ maxWidth: "600px", textAlign: "center" }}>
+          <Title order={1}>Scheduled Maintenance</Title>
+          <Text color="white" size="sm">
+            Our platform is currently undergoing scheduled maintenance today. During this
+            time, the Portal UI is temporarily unavailable, and users are unable to create
+            or edit applications, adjust security settings, or pay plans. However, rest
+            assured that all relay requests are being processed as usual.
+          </Text>
+          <Group position="center">
+            <Button component={Link} size="sm" to="/">
+              Portal Home
+            </Button>
+          </Group>
+        </Box>
+      </Center>
+    )
+  }
+
+  return <Outlet />
 }
 
 export const CatchBoundary = () => {

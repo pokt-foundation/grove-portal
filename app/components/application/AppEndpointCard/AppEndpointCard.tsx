5c61d1921c41227a0a562971e36613e00e72eced
r1ck12 <rickregien@gmail.com>
2023-05-03 11:23:56 -0300

    refactor: remove Covalent from this branch

--- app/components/application/AppEndpointCard/AppEndpointCard.tsx
+++ app/components/application/AppEndpointCard/AppEndpointCard.tsx
@@ -1,12 +1,6 @@
 import {
-  Anchor,
-  Box,
-  Button,
   Text,
   useMantineTheme,
-  Grid,
-  Group,
-  IconArrowUpRight,
 } from "@pokt-foundation/pocket-blocks"
 import { useEffect, useMemo } from "react"
 import AppEndpointUrl, {
@@ -14,9 +8,6 @@ import AppEndpointUrl, {
 } from "~/components/application/AppEndpointUrl"
 import { Card, links as CardLinks } from "~/components/shared/Card"
 import ChainsDropdown from "~/components/shared/ChainsDropdown/ChainsDropdown"
-import CopyText from "~/components/shared/CopyText"
-import HelpTooltip from "~/components/shared/HelpTooltip"
-import TextInput from "~/components/shared/TextInput"
 import { useUser } from "~/context/UserContext"
 import { Blockchain, BlockchainsQuery, EndpointQuery } from "~/models/portal/sdk"
 import { ChainMetadata, prefixFromChainId } from "~/utils/chainUtils"
@@ -142,63 +133,7 @@ export default function AppEndpointCard({ app, blockchains }: AppEndpointProps) 
               />
             )
           })}
-        <Grid
-          align="center"
-          dir="column"
-          justify="space-between"
-          sx={{
-            margin: "2em 0 0",
-            paddingTop: "2em",
-            borderTop: "1px solid #2F373E",
-          }}
-        >
-          <Grid align="center" justify="space-between" m={0} w="100%">
-            <Group spacing={4}>
-              <Text size="lg" weight={600}>
-                Your Data API
-              </Text>
-              <Text size="sm" weight={400}>
-                (powered by Covalent)
-              </Text>
-              <HelpTooltip
-                label={"This endpoint supports Ethereum, Polygon, BSC and more."}
-              />
-            </Group>
-            <Anchor href="https://www.covalenthq.com/docs/api/" target="_blank">
-              <Button size="xs" variant="outline">
-                <Text fz="xs" mr="0.5em">
-                  Data API Docs
-                </Text>
-                <IconArrowUpRight fill={theme?.colors?.blue[5]} height={18} width={18} />
-              </Button>
-            </Anchor>
-          </Grid>
-          <Box>
-            <Text mb={0}>URL</Text>
-            <TextInput value="https://services.portal.pokt.network/indexing">
-              <CopyText text={String("https://services.portal.pokt.network/indexing")} />
-            </TextInput>
-            <Text mb={0}>API Key</Text>
-            <TextInput
-              mt="0.5em"
-              value={
-                app.integrations.covalentAPIKeyPaid || app.integrations.covalentAPIKeyFree
-              }
-            >
-              <CopyText
-                text={String(
-                  app.integrations.covalentAPIKeyPaid ||
-                    app.integrations.covalentAPIKeyFree,
-                )}
-              />
-            </TextInput>
-            <Text fz="sm" mb="0">
-              The following API key can be uses to authenticate against the Covalent API.
-              When authenticating, add the API key using the x-api-key header to the
-              request.
-            </Text>
-          </Box>
-        </Grid>
+        
       </Card>
     </div>
   )

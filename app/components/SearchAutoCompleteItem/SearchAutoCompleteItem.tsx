5d76a66c196abbd53f4636de7dbaf5c7990fe35a
RabeeAbuBaker <rabee.abubaker@gmail.com>
2023-05-16 20:18:34 +0300

    refactor: Refactoring SearchAutoCompleteItem to use Flex and prefetch routes

--- app/components/SearchAutoCompleteItem/SearchAutoCompleteItem.tsx
+++ app/components/SearchAutoCompleteItem/SearchAutoCompleteItem.tsx
@@ -1,4 +1,5 @@
-import { Box, SelectItemProps, Text } from "@pokt-foundation/pocket-blocks"
+import { Flex, SelectItemProps, Text } from "@pokt-foundation/pocket-blocks"
+import { Link } from "@remix-run/react"
 import { forwardRef } from "react"
 import { documentation } from "~/models/cms/sdk"
 
@@ -6,29 +7,20 @@ type ItemProps = Pick<documentation, "translations" | "id" | "status" | "slug"> 
   SelectItemProps
 
 const SearchAutoCompleteItem = forwardRef<HTMLDivElement, ItemProps>(
-  ({ id, translations, ...others }: ItemProps, ref) => {
-    return (
-      <div ref={ref} {...others}>
-        <Box
-          // TODO: Use Flex component from blocks
-          sx={{
-            display: "flex",
-            alignItems: "flex-start",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            flexWrap: "wrap",
-          }}
-        >
-          <Text color="#FAFAFA" mb={3} mt={0} size={18} weight="bold">
+  ({ id, translations, slug, ...others }: ItemProps, ref) => (
+    <div ref={ref} {...others}>
+      <Link prefetch="intent" to={slug ? slug : "/"}>
+        <Flex align="flex-start" direction="column" justify="start" wrap="wrap">
+          <Text mb={3} mt={0} size={18} weight="bold">
             {translations ? translations[0]?.title : ""}
           </Text>
-          <Text color="#FAFAFA" m="0" size={13.5}>
+          <Text m="0" size={13.5}>
             {translations ? translations[0]?.summary : ""}
           </Text>
-        </Box>
-      </div>
-    )
-  },
+        </Flex>
+      </Link>
+    </div>
+  ),
 )
 
 SearchAutoCompleteItem.displayName = "AutoCompleteSearchItem"

a42d513de663ca964ffce1110e90ebcb99f95dc5
PseudoMera <albinesteban@outlook.com>
2023-05-17 13:55:42 -0400

    feat: pass parent slug to childrens so they can form a nested route

--- app/utils/docs.ts
+++ app/utils/docs.ts
@@ -1,17 +1,24 @@
 import { LinksGroupProps } from "~/components/LinksGroup/LinksGroup"
 import { documentation } from "~/models/cms/sdk"
 
-export const findChildren = (docId: string, docs: documentation[]): LinksGroupProps[] => {
+export const findChildren = (
+  docId: string,
+  docs: documentation[],
+  parentSlug: string = "",
+): LinksGroupProps[] => {
   return docs
     .filter((doc) => doc.parent?.id === docId)
-    .map((doc) => ({
-      id: doc.id,
-      label: doc.translations?.[0]?.title || "",
-      link: `${doc.parent?.slug}/${doc.slug}` || "",
-      slug: doc.slug || "",
-      links: findChildren(doc.id, docs),
-      hasParent: true,
-    }))
+    .map((doc) => {
+      const slugPath = `${parentSlug}/${doc.slug}`
+      return {
+        id: doc.id,
+        label: doc.translations?.[0]?.title || "",
+        link: slugPath || "",
+        slug: doc.slug || "",
+        links: findChildren(doc.id, docs, slugPath),
+        hasParent: true,
+      }
+    })
 }
 
 export const organizeData = (docs: documentation[]): LinksGroupProps[] => {
@@ -22,7 +29,7 @@ export const organizeData = (docs: documentation[]): LinksGroupProps[] => {
       label: doc.translations?.[0]?.title || "",
       link: doc.slug || "",
       slug: doc.slug || "",
-      links: findChildren(doc.id, docs),
+      links: findChildren(doc.id, docs, doc.slug!),
       hasParent: false,
     }))
 }

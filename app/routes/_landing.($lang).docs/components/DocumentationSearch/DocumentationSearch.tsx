7331971a38d55d0916dda7f8c01170cd21084bbe
PseudoMera <albinesteban@outlook.com>
2023-05-24 14:24:01 -0400

    feat: add search mobile version with transition animation

--- app/routes/_landing.($lang).docs/components/DocumentationSearch/DocumentationSearch.tsx
+++ app/routes/_landing.($lang).docs/components/DocumentationSearch/DocumentationSearch.tsx
@@ -2,8 +2,10 @@ import {
   Autocomplete,
   AutocompleteItem,
   IconSearch,
+  MediaQuery,
 } from "@pokt-foundation/pocket-blocks"
 import { useFetcher, useNavigate } from "@remix-run/react"
+import { useState } from "react"
 import { LinksGroupProps } from "~/components/LinksGroup/LinksGroup"
 import AutoCompleteSearchItem from "~/components/SearchAutoCompleteItem"
 import { useDocumentationSearch } from "~/routes/_landing.($lang).docs/hooks"
@@ -15,6 +17,7 @@ interface DocumentationSearchProps {
 export const DocumentationSearch = ({ docsLinks }: DocumentationSearchProps) => {
   const navigate = useNavigate()
   const fetcher = useFetcher()
+  const [mobileExpanded, setMobileExpanded] = useState(false)
 
   const {
     searchResultsData,
@@ -26,59 +29,122 @@ export const DocumentationSearch = ({ docsLinks }: DocumentationSearchProps) => 
   } = useDocumentationSearch({ fetcher, docsLinks })
 
   return (
-    <Autocomplete
-      aria-label="Documentation search"
-      data={fetcher.state === "submitting" ? [] : searchResultsData}
-      dropdownComponent="div"
-      filter={(value, item) => Boolean(item.value)}
-      icon={<IconSearch />}
-      itemComponent={AutoCompleteSearchItem}
-      limit={20}
-      nothingFound={nothingFoundText}
-      placeholder="Search in Docs"
-      rightSection={autocompleteRightSection}
-      rightSectionWidth={25}
-      size="md"
-      styles={{
-        rightSection: {
-          paddingRight: 5,
-        },
-      }}
-      sx={(theme) => ({
-        width: "560px",
-        ".mantine-Autocomplete-itemsWrapper": {
-          maxHeight: "500px",
-        },
-        ".mantine-Autocomplete-wrapper": {
-          display: "flex",
-          justifyContent: "flex-end",
-        },
-        ".mantine-Autocomplete-input": {
-          width: "350px",
-          backgroundColor: "transparent",
-          borderColor: theme.colors.gray[4],
+    <>
+      <MediaQuery largerThan="md" styles={{ display: "none" }}>
+        <Autocomplete
+          aria-label="Documentation search"
+          data={fetcher.state === "submitting" ? [] : searchResultsData}
+          dropdownComponent="div"
+          filter={(_, item) => Boolean(item.value)}
+          icon={<IconSearch />}
+          itemComponent={AutoCompleteSearchItem}
+          limit={20}
+          nothingFound={nothingFoundText}
+          placeholder="Search in Docs"
+          rightSection={autocompleteRightSection}
+          rightSectionWidth={25}
+          size="md"
+          styles={{
+            rightSection: {
+              paddingRight: 5,
+            },
+          }}
+          sx={(theme) => ({
+            width: "280px",
+            ".mantine-Autocomplete-itemsWrapper": {
+              maxHeight: "500px",
+            },
+            ".mantine-Autocomplete-wrapper": {
+              display: "flex",
+              justifyContent: "flex-end",
+            },
+            ".mantine-Autocomplete-input": {
+              width: mobileExpanded ? "280px" : "32px",
+              backgroundColor: "transparent",
+              borderColor: theme.colors.gray[4],
+              transition: "all 0.5s ease-in-out",
 
-          "&::placeholder": {
-            color: theme.colors.gray[4],
-          },
-        },
-        ".mantine-Autocomplete-icon": {
-          color: theme.colors.gray[3],
-        },
-        ".mantine-Autocomplete-dropdown": {
-          backgroundColor: theme.colors.navy[6],
-        },
-      })}
-      value={searchTerm}
-      onChange={(term) => {
-        setSearchResults([])
-        setSearchTerm(term)
-      }}
-      onItemSubmit={(item: AutocompleteItem) => {
-        navigate(item.link)
-        setSearchTerm("")
-      }}
-    />
+              "&::placeholder": {
+                color: theme.colors.gray[4],
+              },
+            },
+            ".mantine-Autocomplete-icon": {
+              color: theme.colors.gray[3],
+            },
+            ".mantine-Autocomplete-dropdown": {
+              backgroundColor: theme.colors.navy[6],
+            },
+          })}
+          value={searchTerm}
+          onChange={(term) => {
+            setSearchResults([])
+            setSearchTerm(term)
+          }}
+          onClick={() => setMobileExpanded(true)}
+          onDropdownClose={() => setMobileExpanded(false)}
+          onItemSubmit={(item: AutocompleteItem) => {
+            navigate(item.link)
+            setSearchTerm("")
+          }}
+        />
+      </MediaQuery>
+
+      <MediaQuery smallerThan="md" styles={{ display: "none" }}>
+        <Autocomplete
+          aria-label="Documentation search"
+          data={fetcher.state === "submitting" ? [] : searchResultsData}
+          dropdownComponent="div"
+          filter={(_, item) => Boolean(item.value)}
+          icon={<IconSearch />}
+          itemComponent={AutoCompleteSearchItem}
+          limit={20}
+          nothingFound={nothingFoundText}
+          placeholder="Search in Docs"
+          rightSection={autocompleteRightSection}
+          rightSectionWidth={25}
+          size="md"
+          styles={{
+            rightSection: {
+              paddingRight: 5,
+            },
+          }}
+          sx={(theme) => ({
+            width: "560px",
+            ".mantine-Autocomplete-itemsWrapper": {
+              maxHeight: "500px",
+            },
+            ".mantine-Autocomplete-wrapper": {
+              display: "flex",
+              justifyContent: "flex-end",
+            },
+            ".mantine-Autocomplete-input": {
+              width: "350px",
+              backgroundColor: "transparent",
+              borderColor: theme.colors.gray[4],
+
+              "&::placeholder": {
+                color: theme.colors.gray[4],
+              },
+            },
+            ".mantine-Autocomplete-icon": {
+              color: theme.colors.gray[3],
+            },
+            ".mantine-Autocomplete-dropdown": {
+              backgroundColor: theme.colors.navy[6],
+            },
+          })}
+          value={searchTerm}
+          onChange={(term) => {
+            setSearchResults([])
+            setSearchTerm(term)
+          }}
+          onItemSubmit={(item: AutocompleteItem) => {
+            navigate(item.link)
+            setSearchTerm("")
+          }}
+        />
+      </MediaQuery>
+    </>
   )
 }
 

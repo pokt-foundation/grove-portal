3e2d3d84856de0847b4efab00236e19f825ebcfd
r1ck12 <rickregien@gmail.com>
2023-05-25 12:56:58 -0300

    test: add Table tests

---
+++ app/components/Table/Table.test.tsx
@@ -0,0 +1,51 @@
+import "@testing-library/jest-dom"
+import { render } from "@testing-library/react"
+import { Table } from "./Table"
+
+describe("Table component", () => {
+  const mockData = [
+    { id: 1, name: "Mock Item 1" },
+    { id: 2, name: "Mock Item 2" },
+  ]
+  const mockColumns = ["id", "name"]
+
+  it("renders without crashing", () => {
+    const { container } = render(
+      <Table
+        data={mockData}
+        columns={mockColumns}
+        label={"Test Label"}
+        paginate={true}
+        search={true}
+      />,
+    )
+    expect(container.firstChild).toHaveClass("pokt-table")
+  })
+
+  it("receives necessary props", () => {
+    const { getByText } = render(
+      <Table
+        data={mockData}
+        columns={mockColumns}
+        label={"Test Label"}
+        paginate={true}
+        search={true}
+      />,
+    )
+    expect(getByText("Test Label")).toBeInTheDocument()
+  })
+
+  it("renders the correct number of rows", () => {
+    const { getAllByRole } = render(
+      <Table
+        data={mockData}
+        columns={mockColumns}
+        label={"Test Label"}
+        paginate={true}
+        search={true}
+      />,
+    )
+    const rows = getAllByRole("row")
+    expect(rows).toHaveLength(mockData.length + 1)
+  })
+})

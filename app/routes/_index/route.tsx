6cd5a57986793636c30cc6689bc562c4fed0ee80
RabeeAbuBaker <rabee.abubaker@gmail.com>
2023-09-22 02:05:21 +0300

    refactor: adding missing arg

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -4,5 +4,5 @@ import { redirectToUserAccount, requireUser } from "~/utils/user.server"
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await requireUser(request)
 
-  return redirectToUserAccount(user.accessToken)
+  return redirectToUserAccount(user.accessToken, user.user.portalUserID)
 }

3b11e84a411c2f1476d2ea2300ea6b3498e022e7
RabeeAbuBaker <rabee.abubaker@gmail.com>
2023-09-28 03:25:05 +0300

    refactor: remove feature flags

--- app/root/components/RootProviders/RootProviders.tsx
+++ app/root/components/RootProviders/RootProviders.tsx
@@ -2,7 +2,6 @@ import { ModalsProvider } from "@mantine/modals"
 import { NotificationsProvider } from "@mantine/notifications"
 import { MantineProvider } from "@pokt-foundation/pocket-blocks"
 import React from "react"
-import { FeatureFlagsContextProvider } from "~/context/FeatureFlagContext"
 import { portalTheme } from "~/root/portalTheme"
 
 const RootProviders = ({ children }: { children: React.ReactNode }) => {
@@ -13,11 +12,9 @@ const RootProviders = ({ children }: { children: React.ReactNode }) => {
       withNormalizeCSS
       theme={portalTheme}
     >
-      <FeatureFlagsContextProvider>
-        <NotificationsProvider position="bottom-center">
-          <ModalsProvider>{children}</ModalsProvider>
-        </NotificationsProvider>
-      </FeatureFlagsContextProvider>
+      <NotificationsProvider position="bottom-center">
+        <ModalsProvider>{children}</ModalsProvider>
+      </NotificationsProvider>
     </MantineProvider>
   )
 }
--- app/routes/admin/route.tsx
+++ app/routes/admin/route.tsx
@@ -1,7 +1,6 @@
-import { Grid, Space, Container, Group, Box, Title } from "@pokt-foundation/pocket-blocks"
+import { Space, Container } from "@pokt-foundation/pocket-blocks"
 import { LoaderFunction, json, LinksFunction } from "@remix-run/node"
-import { NavLink, Outlet } from "@remix-run/react"
-import clsx from "clsx"
+import { Outlet } from "@remix-run/react"
 import styles from "./styles.css"
 import { User } from "~/models/portal/sdk"
 import { requireAdmin } from "~/utils/user.server"
@@ -23,57 +22,11 @@ export const loader: LoaderFunction = async ({ request }) => {
   })
 }
 
-type Route = {
-  to: string
-  label?: string
-  icon?: React.ReactNode
-  end?: boolean
-  external?: boolean
-}
-
 export default function Admin() {
-  const routes: Route[] = [
-    {
-      to: "users/pay-plan",
-      label: "Pay Plan",
-    },
-    {
-      to: "settings/feature-flags",
-      label: "Feature Flags",
-    },
-  ]
-
   return (
     <Container size="md">
       <Space h={60} />
-      <Grid>
-        <Grid.Col sm={3}>
-          <Group>
-            <Box>
-              <Title order={3}>Admin Nav</Title>
-            </Box>
-            <ul className="admin-nav">
-              {routes.map((route) => (
-                <li key={route.to}>
-                  <NavLink
-                    className={({ isActive }) =>
-                      clsx("nav-link", { "nav-link-active": isActive })
-                    }
-                    end={true}
-                    to={route.to}
-                  >
-                    {route.label}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </Group>
-        </Grid.Col>
-        <Grid.Col sm={9}>
-          <Outlet />
-        </Grid.Col>
-      </Grid>
-      <Space h={60} />
+      <Outlet />
     </Container>
   )
 }

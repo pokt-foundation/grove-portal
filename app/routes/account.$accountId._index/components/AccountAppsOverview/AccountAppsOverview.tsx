cb26f52c26b5e8c0fef51c61713f8522c41c378e
RabeeAbuBaker <rabee.abubaker@gmail.com>
2023-10-11 21:45:06 +0300

    refactor: Application Overview updates

--- app/routes/account.$accountId._index/components/AccountAppsOverview/AccountAppsOverview.tsx
+++ app/routes/account.$accountId._index/components/AccountAppsOverview/AccountAppsOverview.tsx
@@ -7,14 +7,35 @@ import {
   Text,
 } from "@pokt-foundation/pocket-blocks"
 import { AnalyticsRelaysAggregated } from "~/models/dwh/sdk/models/AnalyticsRelaysAggregated"
+import { getTotalErrors } from "~/utils/chartUtils"
+import { commify } from "~/utils/formattingUtils"
 
 const labels: Record<keyof AnalyticsRelaysAggregated, string> = {
   avgLatency: "Average Latency",
   countTotal: "Total Relays",
   rateSuccess: "Success Rate",
-  rateError: "Error Rate",
+  rateError: "Total Errors",
   categoryValue: "Account",
   date: "Date",
+}
+
+const getFormattedValue = (
+  aggregate: AnalyticsRelaysAggregated,
+  key: keyof AnalyticsRelaysAggregated,
+) => {
+  if (!aggregate) return "-"
+
+  switch (key) {
+    case "avgLatency":
+      return `${aggregate[key]}ms`
+    case "rateSuccess":
+      return `${aggregate[key]}%`
+    case "rateError":
+      const errorsTotal = getTotalErrors(aggregate)
+      return String(errorsTotal ? commify(errorsTotal) : "-")
+    default:
+      return commify(String(aggregate[key]))
+  }
 }
 
 const order: (keyof AnalyticsRelaysAggregated)[] = [
@@ -67,7 +88,7 @@ export const AccountAppsOverview = ({
           <Box key={key} className={classes.stat}>
             <Stack align="center" spacing={0}>
               <Text fw={600} fz="md">
-                {isLoading ? <Loader size="sm" /> : String(aggregate[key])}
+                {isLoading ? <Loader size="sm" /> : getFormattedValue(aggregate, key)}
               </Text>
               <Text>{labels[key as keyof AnalyticsRelaysAggregated]}</Text>
             </Stack>

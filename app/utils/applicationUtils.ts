185eb802e2536abfcc22a218e197a77aa2cf32e7
RabeeAbuBaker <rabee.abubaker@gmail.com>
2023-10-18 20:49:12 +0300

    refactor: Update apps queries and align usage based on the new fields

--- app/utils/applicationUtils.ts
+++ app/utils/applicationUtils.ts
@@ -1,29 +1,11 @@
-import { PortalApp } from "~/models/portal/sdk"
+import { PortalAppUser } from "~/models/portal/sdk"
 
-export const getAppAcceptedValue = (app: PortalApp, userId: string) => {
-  const user = app.users.find((user) => user.userID === userId)
-
-  if (user) {
-    const acceptedEntry = user.portalAppsAccepted.find(
-      (entry) => entry.portalAppID === app.id,
-    )
-
-    return acceptedEntry ? acceptedEntry.accepted : false
-  }
-
-  return false
+export const getAppAcceptedValue = (portalAppUsers: PortalAppUser[], userId: string) => {
+  const user = portalAppUsers.find((user) => user.portalAppUserID === userId)
+  return user?.accepted ?? false
 }
 
-export const getUserRole = (app: PortalApp, userId: string) => {
-  const user = app.users.find((user) => user.userID === userId)
-
-  if (user) {
-    const role = user.portalAppRoles.find((role) => role.portalAppID === app.id)
-
-    if (role) {
-      return role.roleName
-    }
-  }
-
-  return null
+export const getUserRole = (portalAppUsers: PortalAppUser[], userId: string) => {
+  const user = portalAppUsers.find((user) => user.portalAppUserID === userId)
+  return user?.roleName ?? null
 }

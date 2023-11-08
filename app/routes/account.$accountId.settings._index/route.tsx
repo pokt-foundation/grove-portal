bfbe3bf05661e6c98524626567c0c1e49afedfa0
RabeeAbuBaker <rabee.abubaker@gmail.com>
2023-11-09 01:36:40 +0200

    feat: Adding account settings route & view

---
+++ app/routes/account.$accountId.settings._index/route.tsx
@@ -0,0 +1,17 @@
+import { MetaFunction } from "@remix-run/node"
+import { useOutletContext } from "@remix-run/react"
+import { AccountIdLoaderData } from "~/routes/account.$accountId/route"
+import AccountSettingsView from "~/routes/account.$accountId.settings._index/view"
+import { seo_title_append } from "~/utils/seo"
+
+export const meta: MetaFunction = () => {
+  return {
+    title: `Account Settings ${seo_title_append}`,
+  }
+}
+
+export default function AccountSettings() {
+  const { account } = useOutletContext<AccountIdLoaderData>()
+
+  return <AccountSettingsView account={account} />
+}

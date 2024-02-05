e3d4e8c7988f2c3598cdb97796f95e01edcf7b2d
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-02-05 23:27:41 +0200

    fix: Using correct emotionCache in entry.client

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,10 +1,11 @@
+import { createEmotionCache } from "@mantine/core"
 import { ClientProvider } from "@mantine/remix"
 import { RemixBrowser } from "@remix-run/react"
 import { startTransition, StrictMode } from "react"
 import { hydrateRoot } from "react-dom/client"
-import { emotionCache } from "~/utils/mantineCache"
 
 startTransition(() => {
+  const emotionCache = createEmotionCache({ key: "mantine" })
   hydrateRoot(
     document,
     <StrictMode>

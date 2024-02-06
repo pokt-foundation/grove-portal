0565fbd61c58d21dacd2079c17d9ecceea29a57d
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-02-06 15:00:36 +0200

    feat: Switching back to hydrate for now 🥹

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,17 +1,11 @@
-import { createEmotionCache } from "@mantine/core"
 import { ClientProvider } from "@mantine/remix"
 import { RemixBrowser } from "@remix-run/react"
-import { startTransition, StrictMode } from "react"
-import { hydrateRoot } from "react-dom/client"
+import { hydrate } from "react-dom"
+import { emotionCache } from "~/utils/mantineCache.js"
 
-startTransition(() => {
-  const emotionCache = createEmotionCache({ key: "mantine" })
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <ClientProvider emotionCache={emotionCache}>
-        <RemixBrowser />
-      </ClientProvider>
-    </StrictMode>,
-  )
-})
+hydrate(
+  <ClientProvider emotionCache={emotionCache}>
+    <RemixBrowser />
+  </ClientProvider>,
+  document,
+)

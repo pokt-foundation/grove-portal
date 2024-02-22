4be0b9bfe2d52b404e3e94daead942c14402a93e
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-02-22 22:00:23 +0200

    refactor: Updating error image alt - Adding TODO

--- app/components/ErrorBoundaryView/ErrorBoundaryView.tsx
+++ app/components/ErrorBoundaryView/ErrorBoundaryView.tsx
@@ -6,8 +6,9 @@ export const ErrorBoundaryView = () => {
 
   return (
     <EmptyState
-      alt="App limit exceeded"
+      alt="Something went wrong"
       imgHeight={216}
+      // TODO: Replace with Error asset
       imgSrc="/app-limit-exceeded.svg"
       imgWidth={270}
       subtitle={errorMessage}

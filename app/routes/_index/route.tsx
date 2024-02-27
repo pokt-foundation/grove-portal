375622575aae113ba1b2d1b83f489454c713a4a3
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-02-27 15:03:03 +0200

    feat: Adding root route ErrorBoundary

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,7 +1,13 @@
 import { LoaderFunction } from "@remix-run/node"
+import React from "react"
+import ErrorBoundaryView from "~/components/ErrorBoundaryView/ErrorBoundaryView"
 import { redirectToUserAccount, requireUser } from "~/utils/user.server"
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await requireUser(request)
   return redirectToUserAccount(user)
+}
+
+export function ErrorBoundary() {
+  return <ErrorBoundaryView />
 }

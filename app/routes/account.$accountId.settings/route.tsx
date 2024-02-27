fc8148d0a228fe1e20e0716cfefea34842fe301d
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-02-27 15:32:59 +0200

    feat: Adding ErrorBoundary for account settings root route

--- app/routes/account.$accountId.settings/route.tsx
+++ app/routes/account.$accountId.settings/route.tsx
@@ -1,29 +1,26 @@
 import { Outlet, useActionData, useRouteLoaderData } from "@remix-run/react"
 import React from "react"
-import ErrorView from "~/components/ErrorView"
+import ErrorBoundaryView from "~/components/ErrorBoundaryView/ErrorBoundaryView"
 import useActionNotification from "~/hooks/useActionNotification"
 import { AccountIdLoaderData } from "~/routes/account.$accountId/route"
 import AccountSettingsLayoutView from "~/routes/account.$accountId.settings/view"
-import { DataStruct } from "~/types/global"
+import { ActionDataStruct } from "~/types/global"
 
 export default function AccountSettings() {
-  const { data, error, message } = useRouteLoaderData(
+  const { account, accounts, user, userRole } = useRouteLoaderData(
     "routes/account.$accountId",
-  ) as DataStruct<AccountIdLoaderData>
-  const actionData = useActionData() as DataStruct<{ success: boolean }>
+  ) as AccountIdLoaderData
+  const actionData = useActionData() as ActionDataStruct<{ success: boolean }>
 
   // handle all notifications at the layout level
   useActionNotification(actionData)
-
-  if (error) {
-    return <ErrorView message={message} />
-  }
-
-  const { account, accounts, user, userRole } = data
 
   return (
     <AccountSettingsLayoutView account={account}>
       <Outlet context={{ account, accounts, user, userRole }} />
     </AccountSettingsLayoutView>
   )
+}
+export function ErrorBoundary() {
+  return <ErrorBoundaryView />
 }

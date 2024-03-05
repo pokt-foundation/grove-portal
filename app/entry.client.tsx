1c9aad4b2ea8eaa1f288e141b561a0992aa36a99
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-03-06 01:09:11 +0200

    feat: Updating entry.client and entry.server - Removing emotion cache - Using RemixV2 default template entry.server

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,5 @@
-import { ClientProvider } from "@mantine/remix"
 import { RemixBrowser } from "@remix-run/react"
 import { hydrate } from "react-dom"
-import { emotionCache } from "~/utils/mantineCache.js"
 
-hydrate(
-  <ClientProvider emotionCache={emotionCache}>
-    <RemixBrowser />
-  </ClientProvider>,
-  document,
-)
+hydrate(<RemixBrowser />, document)
+
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,34 +1,122 @@
-import { CacheProvider } from "@emotion/react"
-import { createStylesServer, injectStyles } from "@mantine/remix"
-import type { EntryContext } from "@remix-run/node"
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.server
+ */
+
+import { PassThrough } from "node:stream"
+
+import type { AppLoadContext, EntryContext } from "@remix-run/node"
+import { createReadableStreamFromReadable } from "@remix-run/node"
 import { RemixServer } from "@remix-run/react"
-import { renderToString } from "react-dom/server"
+import { isbot } from "isbot"
+import { renderToPipeableStream } from "react-dom/server"
 import { getClientEnv } from "~/utils/environment.server"
-import { emotionCache } from "~/utils/mantineCache"
 
 global.ENV = getClientEnv()
 
-const server = createStylesServer(emotionCache)
+const ABORT_DELAY = 5_000
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
+  loadContext: AppLoadContext,
 ) {
-  // Wrap the server in the cache provider and set the cache
-  let markup = renderToString(
-    <CacheProvider value={emotionCache}>
-      <RemixServer context={remixContext} url={request.url} />
-    </CacheProvider>,
-  )
-  if (process.env.NODE_ENV !== "production") {
-    responseHeaders.set("Cache-Control", "no-store")
-  }
-  responseHeaders.set("Content-Type", "text/html")
+  return isbot(request.headers.get("user-agent"))
+    ? handleBotRequest(request, responseStatusCode, responseHeaders, remixContext)
+    : handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext)
+}
 
-  return new Response(`<!DOCTYPE html>${injectStyles(markup, server)}`, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+function handleBotRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer abortDelay={ABORT_DELAY} context={remixContext} url={request.url} />,
+      {
+        onAllReady() {
+          shellRendered = true
+          const body = new PassThrough()
+          const stream = createReadableStreamFromReadable(body)
+
+          responseHeaders.set("Content-Type", "text/html")
+
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            }),
+          )
+
+          pipe(body)
+        },
+        onShellError(error: unknown) {
+          reject(error)
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error)
+          }
+        },
+      },
+    )
+
+    setTimeout(abort, ABORT_DELAY)
+  })
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer abortDelay={ABORT_DELAY} context={remixContext} url={request.url} />,
+      {
+        onShellReady() {
+          shellRendered = true
+          const body = new PassThrough()
+          const stream = createReadableStreamFromReadable(body)
+
+          responseHeaders.set("Content-Type", "text/html")
+
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            }),
+          )
+
+          pipe(body)
+        },
+        onShellError(error: unknown) {
+          reject(error)
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error)
+          }
+        },
+      },
+    )
+
+    setTimeout(abort, ABORT_DELAY)
   })
 }

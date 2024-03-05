a91a6374867e740f7accc068714959572d57b84e
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-03-06 01:07:13 +0200

    feat: Updating root providers and Document, adding support for theme caching and preventing FOUC (flash of unstyled content)

--- app/root/components/RootProviders/RootProviders.tsx
+++ app/root/components/RootProviders/RootProviders.tsx
@@ -3,20 +3,15 @@ import { ModalsProvider } from "@mantine/modals"
 import { Notifications } from "@mantine/notifications"
 import React from "react"
 import { portalTheme } from "~/root/portalTheme"
-import { emotionCache } from "~/utils/mantineCache"
 
 const RootProviders = ({ children }: { children: React.ReactNode }) => {
   return (
-    <MantineProvider
-      withCSSVariables
-      withGlobalStyles
-      withNormalizeCSS
-      emotionCache={emotionCache}
-      theme={portalTheme}
-    >
-      <Notifications position="bottom-center" />
-      <ModalsProvider>{children}</ModalsProvider>
-    </MantineProvider>
+    <>
+      <MantineProvider defaultColorScheme="dark" theme={portalTheme}>
+        <Notifications position="bottom-center" />
+        <ModalsProvider>{children}</ModalsProvider>
+      </MantineProvider>
+    </>
   )
 }
 

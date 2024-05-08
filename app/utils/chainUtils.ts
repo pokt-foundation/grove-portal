f9368e4f7d2c9c3009b06ad3908445d14081e668
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-05-08 20:34:39 +0300

    refactor: add docs links

--- app/utils/chainUtils.ts
+++ app/utils/chainUtils.ts
@@ -52,6 +52,9 @@ export const CHAIN_DOCS_URL: KeyValuePair<string> = {
   "sui-mainnet": "sui-api/intro",
   "scroll-mainnet": "scroll-api/intro",
   "scroll-testnet": "scroll-api/intro",
+  "zksync-era-mainnet": "zksync-era-api/intro",
+  "blast-archival": "blastchain-api/intro",
+  "opbnb-archival": "binance-smart-chain-api/endpoints/opBNB-archival",
 }
 
 export const evmChains = [

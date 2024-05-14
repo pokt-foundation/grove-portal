a6df8686ef3db06053a9d1aecac4eea0ef82df9f
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-05-14 04:42:38 +0300

    fix: show bank payment under Payment Method section correctly depending on the payment type

--- app/routes/account.$accountId.billing.$invoiceId/components/InvoicePaymentMethod/InvoicePaymentMethod.tsx
+++ app/routes/account.$accountId.billing.$invoiceId/components/InvoicePaymentMethod/InvoicePaymentMethod.tsx
@@ -5,33 +5,71 @@ import { Stripe } from "~/models/stripe/stripe.server"
 import { CHARGE_STATUS_COLOR } from "~/utils/billingUtils"
 
 const InvoicePaymentMethod = ({ charge }: { charge: Stripe.Charge }) => {
-  return (
+  return charge.payment_method_details?.type === "us_bank_account" ||
+    charge.payment_method_details?.type === "card" ? (
     <Stack>
       <Title order={6}>Payment Method</Title>
       <DataTable
         columns={[
           {
-            accessor: "payment_method_details",
-            render: ({ payment_method_details }) => (
-              <Text tt="capitalize">
-                {payment_method_details?.type} ({payment_method_details?.card?.brand})
-              </Text>
-            ),
+            accessor: "payment_method_details.type",
+            render: ({ payment_method_details }) =>
+              payment_method_details?.type === "us_bank_account" ? (
+                <Text tt="capitalize">
+                  {payment_method_details?.us_bank_account?.bank_name ??
+                    "US Bank Account"}
+                </Text>
+              ) : (
+                <Text tt="capitalize">
+                  {payment_method_details?.type} ({payment_method_details?.card?.brand})
+                </Text>
+              ),
             title: "Type",
           },
           {
             accessor: "payment_method_details.card.last4",
             render: ({ payment_method_details }) =>
-              `---- ${payment_method_details?.card?.last4}`,
+              `---- ${
+                payment_method_details?.type === "us_bank_account"
+                  ? payment_method_details?.us_bank_account?.last4
+                  : payment_method_details?.card?.last4
+              }`,
             title: "Number",
           },
-          {
-            accessor: "payment_method_details.card",
-            render: ({ payment_method_details }) =>
-              `${payment_method_details?.card?.exp_month}/
+          ...(charge?.payment_method_details?.type === "us_bank_account"
+            ? [
+                {
+                  accessor: "payment_method_details.us_bank_account.fingerprint",
+                  render: ({
+                    payment_method_details,
+                  }: {
+                    payment_method_details: Stripe.Charge.PaymentMethodDetails | null
+                  }) => `${payment_method_details?.us_bank_account?.fingerprint}`,
+                  title: "Fingerprint",
+                },
+                {
+                  accessor: "payment_method_details.us_bank_account.payment_reference",
+                  render: ({
+                    payment_method_details,
+                  }: {
+                    payment_method_details: Stripe.Charge.PaymentMethodDetails | null
+                  }) => `${payment_method_details?.us_bank_account?.payment_reference}`,
+                  title: "Payment reference",
+                },
+              ]
+            : [
+                {
+                  accessor: "payment_method_details.card.exp_month",
+                  render: ({
+                    payment_method_details,
+                  }: {
+                    payment_method_details: Stripe.Charge.PaymentMethodDetails | null
+                  }) =>
+                    `${payment_method_details?.card?.exp_month}/
                     ${payment_method_details?.card?.exp_year}`,
-            title: "Expires",
-          },
+                  title: "Expires",
+                },
+              ]),
           {
             accessor: "status",
             render: ({ status }) => (
@@ -59,7 +97,7 @@ const InvoicePaymentMethod = ({ charge }: { charge: Stripe.Charge }) => {
         verticalSpacing="sm"
       />
     </Stack>
-  )
+  ) : null
 }
 
 export default InvoicePaymentMethod

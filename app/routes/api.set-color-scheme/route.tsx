3d838cc9cc5fc263aed2f7d043c3642570fa4ad8
RabeeAbuBaker <rabee.abubaker@gmail.com>
2024-05-30 17:59:01 +0300

    fix: type clean up

--- app/routes/api.set-color-scheme/route.tsx
+++ app/routes/api.set-color-scheme/route.tsx
@@ -1,6 +1,6 @@
-import { MantineColorScheme } from "@mantine/core"
 import { ActionFunction, json } from "@remix-run/node"
 import invariant from "tiny-invariant"
+import { ColorScheme } from "~/root"
 import { getErrorMessage } from "~/utils/catchError"
 import { getColorSchemeSession } from "~/utils/colorScheme.server"
 import { requireUser } from "~/utils/user.server"
@@ -9,7 +9,7 @@ export const action: ActionFunction = async ({ request }) => {
   const user = await requireUser(request)
   invariant(user.user.auth0ID && user.user.email, "user not found")
   const formData = await request.formData()
-  const colorScheme = formData.get("color-scheme") as MantineColorScheme
+  const colorScheme = formData.get("color-scheme") as ColorScheme
   const colorSchemeSession = await getColorSchemeSession(request)
 
   invariant(typeof colorScheme === "string", "theme value must be set")
